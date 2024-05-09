6b0f95c57da52fd1846d3d36e7e29e0b656f1ccd
Robert Neumann <git@rnmail.de>
2024-05-09 23:31:40 +0200

    not working last try

--- httpget/sendsms.hs
+++ httpget/sendsms.hs
@@ -1,5 +1,8 @@
 import Network.HTTP.Simple
 import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Char8 as B8
+import Network.HTTP.Types.Header (mk)
+import Network.HTTP.Types.Header (HeaderName)
 
 main :: IO ()
 main = do
@@ -7,10 +10,22 @@ main = do
     let requestBody = "isTest=false&goformId=SEND_SMS&notCallback=true&Number=%2B491622472546&" <>
                       "sms_time=24%3B05%3B08%3B00%3B23%3B21%3B%2B2&MessageBody=48656c6c6f20576f726c64&" <>
                       "ID=-1&encode_type=GSM7_default"
+    --{- 
+    let contentTypeHeaderName :: HeaderName
+    contentTypeHeaderName <- mk "Content-Type"
 
+    let request'' = addRequestHeader contentTypeHeaderName (B8.pack "application/x-www-form-urlencoded") request'
+
+    let request' = setRequestBodyLBS (L8.pack requestBody) request
+    let request'' = addRequestHeader contentTypeHeaderName (B8.pack "application/x-www-form-urlencoded") request'
+    response <- httpLBS request'
+    --}
+
+    
+    {- 
     request <- parseRequest $ "POST " ++ url
     let request' = setRequestBodyLBS (L8.pack requestBody) request
-    response <- httpLBS request'
+    -}
 
     putStrLn $ "Response status code: " ++ show (getResponseStatusCode response)
     putStrLn $ "Response body: " ++ L8.unpack (getResponseBody response)
